b95234bf8b744e1ce99430d8a902d7dbb502446d
praveen.dangwani <praveen.dangwani@clevertap.com>
2023-10-11 16:46:27 +0530

    adding CT SW

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import clevertap from 'clevertap-web-sdk';
 
+clevertap.init('W9R-486-4W5Z')
+clevertap.privacy.push({ optOut: false });
+clevertap.privacy.push({useIP: false})
+clevertap.setLogLevel(3)
 function App() {
+  
+  // const enablePush = () =>{
+  //   clevertap.notifications.push({
+  //     "titleText":"Would you like to receive Push Notifications?",
+  //     "bodyText":"We promise to only send you relevant content and give you updates on your transactions",
+  //     "okButtonText":"Ok",
+  //     "rejectButtonText":"Cancel",
+  //     "okButtonColor":"#F28046",
+  //     "askAgainTimeInSeconds":5,
+  //     "serviceWorkerPath": "./firebase-messaging-sw.js"
+  // });
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Hello World</h1>
+        {/* <button onClick={enablePush}> Enable Push Notification </button> */}
+      </div>
     </div>
   );
 }
