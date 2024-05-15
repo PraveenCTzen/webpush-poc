acc2d0c5af0af6a02b0e27cc9198c7f767fe1024
praveen.dangwani <praveen.dangwani@clevertap.com>
2024-05-15 17:02:58 +0530

    Ct SW

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,6 +2,7 @@
 // importScripts('sw_webpush.js');
 
 // /* eslint-disable */
+importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush_only_registration.js');
 // importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js');// remove CleverTap server worker from your root folder
 // importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 // importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
