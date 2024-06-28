18a4c9690c3811f8080407d1f06c2376fe9596cf
PraveenCTzen <praveen.dangwani@clevertap.com>
2024-06-28 14:43:12 +0530

    Onesignal to CT

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,24 +6,24 @@ import clevertap from 'clevertap-web-sdk';
 
 const Home = () => {
 
-    // const enablePush = () =>{
-    //   console.log('push log');
-    //       clevertap.notifications.push({
-    //         "titleText":"Would you like to receive Push Notifications?",
-    //         "bodyText":"We promise to only send you relevant content and give you updates on your transactions",
-    //         "okButtonText":"Ok",
-    //         "rejectButtonText":"Cancel",
-    //         "okButtonColor":"#F28046",
-    //         "askAgainTimeInSeconds":5,
-    //         "serviceWorkerPath": "./firebase-messaging-sw.js"
-    //     });
-    //     }
+    const enablePush = () =>{
+      console.log('push log');
+          clevertap.notifications.push({
+            "titleText":"Would you like to receive Push Notifications?",
+            "bodyText":"We promise to only send you relevant content and give you updates on your transactions",
+            "okButtonText":"Ok",
+            "rejectButtonText":"Cancel",
+            "okButtonColor":"#F28046",
+            "askAgainTimeInSeconds":5,
+            "serviceWorkerPath": "./firebase-messaging-sw.js"
+        });
+        }
 
   return (
     <div className='homeTop'>
         <h1> Hello World</h1>
         <div className='buttons'>
-        {/* <button className='webPushBtn' onClick={enablePush}> Enable Push Notification </button> */}
+        <button className='webPushBtn' onClick={enablePush}> Enable Push Notification </button>
         <button className='scrollTest'> scroll</button>
         </div>
     </div>
