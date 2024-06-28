6976be6ba442edbed203026754a36a453cc6f038
PraveenCTzen <praveen.dangwani@clevertap.com>
2024-06-28 14:08:04 +0530

    test one signal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { WebRoutes } from './WebRoutes';
+import OneSignal from 'react-onesignal';
 // import clevertap from 'clevertap-web-sdk';
 
 
 function App() {
+  
+  const [initialized, setInitialized] = useState(false);
+  OneSignal.init({ appId: '2fc4c79b-5bf1-49d6-9bae-889cf6ea486a' }).then(() => {
+    setInitialized(true);
+    OneSignal.Slidedown.promptPush();
+  })
+
   // const enablePush = () =>{
   //   clevertap.notifications.push({
   //     "titleText":"Would you like to receive Push Notifications?",
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,23 +6,26 @@ import clevertap from 'clevertap-web-sdk';
 
 const Home = () => {
 
-    const enablePush = () =>{
-      console.log('push log');
-          clevertap.notifications.push({
-            "titleText":"Would you like to receive Push Notifications?",
-            "bodyText":"We promise to only send you relevant content and give you updates on your transactions",
-            "okButtonText":"Ok",
-            "rejectButtonText":"Cancel",
-            "okButtonColor":"#F28046",
-            "askAgainTimeInSeconds":5,
-            "serviceWorkerPath": "./firebase-messaging-sw.js"
-        });
-        }
+    // const enablePush = () =>{
+    //   console.log('push log');
+    //       clevertap.notifications.push({
+    //         "titleText":"Would you like to receive Push Notifications?",
+    //         "bodyText":"We promise to only send you relevant content and give you updates on your transactions",
+    //         "okButtonText":"Ok",
+    //         "rejectButtonText":"Cancel",
+    //         "okButtonColor":"#F28046",
+    //         "askAgainTimeInSeconds":5,
+    //         "serviceWorkerPath": "./firebase-messaging-sw.js"
+    //     });
+    //     }
 
   return (
-    <div>
+    <div className='homeTop'>
         <h1> Hello World</h1>
-        <button onClick={enablePush}> Enable Push Notification </button>
+        <div className='buttons'>
+        {/* <button className='webPushBtn' onClick={enablePush}> Enable Push Notification </button> */}
+        <button className='scrollTest'> scroll</button>
+        </div>
     </div>
   )
 }
