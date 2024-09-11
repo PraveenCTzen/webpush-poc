b80952e39cb213961f6e1bfcdf300c75e7a40a11
PraveenCTzen <praveen.dangwani@clevertap.com>
2024-09-11 16:31:07 +0530

    one signal

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React,{useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { WebRoutes } from './WebRoutes';
-// import OneSignal from 'react-onesignal';
+import OneSignal from 'react-onesignal';
 // import clevertap from 'clevertap-web-sdk';
 
 
 function App() {
   
-  // const [initialized, setInitialized] = useState(false);
-  // OneSignal.init({ appId: '2fc4c79b-5bf1-49d6-9bae-889cf6ea486a' }).then(() => {
-  //   setInitialized(true);
-  //   OneSignal.Slidedown.promptPush();
-  // })
-  // OneSignal.Slidedown.promptPush();
+  const [initialized, setInitialized] = useState(false);
+  OneSignal.init({ appId: '2fc4c79b-5bf1-49d6-9bae-889cf6ea486a' }).then(() => {
+    setInitialized(true);
+    OneSignal.Slidedown.promptPush();
+  })
+  OneSignal.Slidedown.promptPush();
 
   // const enablePush = () =>{
   //   clevertap.notifications.push({
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import clevertap from 'clevertap-web-sdk';
+// import clevertap from 'clevertap-web-sdk';
 
 // import { useState } from 'react';
 
@@ -8,15 +8,15 @@ const Home = () => {
 
     const enablePush = () =>{
       console.log('push log')
-          clevertap.notifications.push({
-            "titleText":"Would you like to receive Push Notifications?",
-            "bodyText":"We promise to only send you relevant content and give you updates on your transactions",
-            "okButtonText":"Ok",
-            "rejectButtonText":"Cancel",
-            "okButtonColor":"#F28046",
-            "askAgainTimeInSeconds":5,
-            "serviceWorkerPath": "./firebase-messaging.js"
-        });
+        //   clevertap.notifications.push({
+        //     "titleText":"Would you like to receive Push Notifications?",
+        //     "bodyText":"We promise to only send you relevant content and give you updates on your transactions",
+        //     "okButtonText":"Ok",
+        //     "rejectButtonText":"Cancel",
+        //     "okButtonColor":"#F28046",
+        //     "askAgainTimeInSeconds":5,
+        //     "serviceWorkerPath": "./firebase-messaging.js"
+        // });
         }
 
   return (
